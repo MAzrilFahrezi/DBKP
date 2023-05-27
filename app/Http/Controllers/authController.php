dc579edef87d6d9ded516f24a7700115ac404133
Muhammad Azril Fahrezi <mazrilfahrezi@mhs.mdp.ac.id>
2023-05-27 08:09:06 +0700

    Update authController.php

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -101,7 +101,7 @@ class authController extends Controller
             'email' => 'string',
             'no_hp' => 'string',
             'is_admin' => 'string',
-            'password' => 'String',
+            'password' => 'string',
         ]);
         
         $user = User::find($id);
